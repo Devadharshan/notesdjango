7426da92b628ded24f86a24bf110f9206d7567ee
Devadharshan <dharshandeva01@gmail.com>
2021-03-13 14:39:46 +0530

    commit one

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Note = ({ title }) => ( <
+    React.Fragment >
+    <
+    a href = "#" > { title } < /a> <
+    /React.Fragment>
+)
+
+export default Note;
---
+++ notes/views.py
@@ -0,0 +1,15 @@
+from django.db.models.query import QuerySet
+from rest_framework import generics
+
+from . import models
+
+from . import serializers
+
+class NoteList(generics.ListCreateAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Note.objects.all()
+    serializer_class = serializers.NoteSerializer
+
